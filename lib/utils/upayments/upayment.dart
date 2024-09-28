368a3f3577b63095110b1ce67de37b9dedb1ec62
khaledawad <kawad852@gmail.com>
2024-09-28 12:35:50 +0300

    new version

--- lib/utils/upayments/upayment.dart
+++ lib/utils/upayments/upayment.dart
@@ -5,6 +5,7 @@ import 'package:bebrain/alerts/loading/app_over_loader.dart';
 import 'package:bebrain/utils/base_extensions.dart';
 import 'package:bebrain/utils/shared_pref.dart';
 import 'package:bebrain/utils/upayments/payment_webview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
@@ -18,12 +19,12 @@ class UPayment {
     bool withOverlayLoader = false,
   }) async {
     try {
-      String url = 'https://sandboxapi.upayments.com/api/v1/charge';
+      String url = kDebugMode ? 'https://sandboxapi.upayments.com/api/v1/charge' : "https://uapi.upayments.com/api/v1/charge";
       Uri uri = Uri.parse(url);
       var headers = {
         'content-type': 'application/json',
         'accept': 'application/json',
-        'Authorization': 'Bearer jtest123',
+        'Authorization': kDebugMode ? 'Bearer jtest123' : "Bearer 90390e121dcf6b37c596d7641534623e73599067",
       };
       var body = jsonEncode(
         {
@@ -49,8 +50,8 @@ class UPayment {
           "customer": {
             "uniqueId": "${MySharedPreferences.user.id}",
             "name": MySharedPreferences.user.name,
-            "email": MySharedPreferences.user.email??"",
-            "mobile": MySharedPreferences.user.phoneNumber??"",
+            "email": MySharedPreferences.user.email ?? "",
+            "mobile": MySharedPreferences.user.phoneNumber ?? "",
           },
           "returnUrl": "https://almosaaed.com/api/upayment/confirm",
           "cancelUrl": "https://almosaaed.com/api/upayment/fail",
@@ -58,7 +59,7 @@ class UPayment {
           "customerExtraData": "User define data"
         },
       );
-      if(withOverlayLoader){
+      if (withOverlayLoader) {
         AppOverlayLoader.show();
       }
       debugPrint("Response:: CheckoutResponse\nUrl:: $url\nheaders:: ${headers.toString()}");
@@ -81,14 +82,14 @@ class UPayment {
           ).then((value) {
             if (value != null) {
               log("success");
-              if(afterPay !=null){
+              if (afterPay != null) {
                 afterPay();
               }
             }
           });
         }
       } else {
-        if(afterPay != null){
+        if (afterPay != null) {
           afterPay();
         }
         AppOverlayLoader.hide();
@@ -98,9 +99,9 @@ class UPayment {
       }
     } catch (e) {
       debugPrint("$e");
-      if(afterPay != null){
-          afterPay();
-        }
+      if (afterPay != null) {
+        afterPay();
+      }
       if (context.mounted) {
         Fluttertoast.showToast(msg: context.appLocalization.generalError);
       }
